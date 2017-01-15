f785e25c6b6e3479493a9798f54de863b437362e
Abel <abel4@msn.com>
2017-01-15 14:24:58 -0600

    Add Ship h/cpp Amended malagaMKII.txt 	- Redifine weapon/ammo upgrade

---
+++ Engine/Ship.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Vector."
+#include "Entity.h"
+#include "Input.h" 
+
+class Ship : public Entity
+{
+public:
+	Ship( Vector Pos, bool IsColliding = false );
+
+	void Draw();
+	void Update();
+
+	bool IsColliding();
+	void DoInput();
+	void ClampToScreenEdges();
+
+public:
+	Vector pos;
+	bool is_colliding;
+};
