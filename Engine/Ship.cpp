38f79d5ff180dbb0c09707ff44c3b2938c8baa35
Abel <abel4@msn.com>
2017-01-17 19:31:04 -0600

    Add a wee bit of Update() code for Ship class

--- Engine/Ship.cpp
+++ Engine/Ship.cpp
@@ -4,10 +4,19 @@ Ship::Ship( Vector Pos, bool IsColliding = false )
 {}
 
 void Ship::Draw()
-{}
+{
+}
 
-void Ship::Update()
-{}
+void Ship::Update( float Dt )
+{
+	if( is_left_pressed || is_right_pressed )
+	{
+		position.x += speed;
+	}
+	else if( position.y >= Amalgum::screen_size.width )
+	{
+		
+}
 
 bool Ship::IsColliding()
 {
